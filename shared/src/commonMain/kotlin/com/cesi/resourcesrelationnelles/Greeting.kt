a64ae614f939d5142f801b06799034712394f68b
Adidou MCN <79933094+Adidooouuu@users.noreply.github.com>
2024-03-15 11:02:04 +0100

    remove all kotlin multi project

--- shared/src/commonMain/kotlin/com/cesi/resourcesrelationnelles/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.cesi.resourcesrelationnelles
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
